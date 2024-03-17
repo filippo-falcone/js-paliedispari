163d8d73bee83c7d4a1c22df5b7187509311966c
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-17 21:17:48 +0100

    feat: add Even Or Odd function

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -7,16 +7,39 @@
    // Creo un alert che stabilisce chi ha vinto
 -------------------------------------------------------------------------------------- */
 /* VARIABLES */
-const userEvenOrOdd = prompt('Scegli tra pari o dispari');
+const userEvenOdd = prompt('Scegli tra pari o dispari');
 const userNumber = parseInt(prompt('Dammi un numero tra 1 e 5'));
 const pcNumber = getRndInteger(1, 5);
+const sum = userNumber + pcNumber;
+const sumEvenOdd = isEvenOdd(sum);
+let userMessage;
+/* APPLICATIONS */
+if (userEvenOdd === sumEvenOdd) {
+    userMessage = 'Hai vinto!';
+} else {
+    userMessage = 'Hai perso!';
+}
 /* OUTPUT */
-console.log('scelta utente:', userEvenOrOdd);
+alert(userMessage);
+console.log('scelta utente:', userEvenOdd);
 console.log('numero utente:', userNumber);
 console.log('numero pc:', pcNumber);
-/* FUNTIONS */
+console.log('somma:', sum);
+/* FUNCTIONS */
 // Funzione per generare un numero random
 // Presa da w3schools
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Funzione per definire se la somma è pari o dispari
+// number: elemento di tipo numerico
+// return: evenOrOdd variabile con il risultato della verifica
+function isEvenOdd(number) {
+    let evenOdd;
+    if (sum % 2 === 0) {
+        evenOdd = 'pari';
+    } else {
+        evenOdd = 'dispari';
+    }
+    return evenOdd;
 }
