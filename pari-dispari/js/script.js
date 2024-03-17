e9ef5a0c96c3e44bc034747a48c1be25bf351acf
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-17 20:52:34 +0100

    feat: add user request and random number function

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,7 +1,22 @@
 /* --------------------------------------------------------------------------------------
-   L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-   Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-   Sommiamo i due numeri
-   Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-   Dichiariamo chi ha vinto.
+   // Chiedo all'utente di sceglire tra pari e dispari
+   // Chiedo di inserire un numero da 1 a 5
+   // Genero un numero casuale sempre da 1 a 5
+   // Sommo i numeri
+   // Creo una funzione per stabilire se la somma è pari o dispari
+   // Creo un alert che stabilisce chi ha vinto
 -------------------------------------------------------------------------------------- */
+/* VARIABLES */
+const userEvenOrOdd = prompt('Scegli tra pari o dispari');
+const userNumber = parseInt(prompt('Dammi un numero tra 1 e 5'));
+const pcNumber = getRndInteger(1, 5);
+/* OUTPUT */
+console.log('scelta utente:', userEvenOrOdd);
+console.log('numero utente:', userNumber);
+console.log('numero pc:', pcNumber);
+/* FUNTIONS */
+// Funzione per generare un numero random
+// Presa da w3schools
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
